f8d344eed8b881346a38372dec289be8ff559edd
Slender <bautista.prieto.hoses@gmail.com>
2025-11-08 18:52:22 -0300

    basic setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,8 @@
+import {createRoot} from "react-dom/client";
+import {StrictMode} from "react";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <h1>Hello, World!</h1>
+  </StrictMode>,
+)
